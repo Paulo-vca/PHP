a6ad66b345dca14dfc86020df9e84c7cb04ae020
Paulo-vca <paulovca26@gmail.com>
2022-08-10 14:33:59 -0400

    subindo

---
+++ calculadora/calculadora.js
@@ -0,0 +1,12 @@
+
+function calculadora() {
+
+    var n1 = document.getElementById('namber1').value;
+    var n2 = document.getElementById('namber2').value;
+    var resultado;
+
+    function soma(){
+            resultado = n1 + n2;
+    }
+
+}
